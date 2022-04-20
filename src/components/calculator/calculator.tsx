7db118d7cadf06c62f3809fd1955b10a9d8a7550
Nikita Pavlienko <pavlienko@macbook.local>
2022-04-20 16:01:50 +0300

    three-added

--- src/components/calculator/calculator.tsx
+++ src/components/calculator/calculator.tsx
@@ -1,6 +1,22 @@
+import * as THREE from "three";
+import * as React from "react"
+import {useRef} from "react";
+import {Canvas} from "@react-three/fiber";
+
+function Box(props: JSX.IntrinsicElements["mesh"]) {
+	const ref = useRef<THREE.Mesh>(null!);
+	return(
+		<mesh {...props} ref={ref}>
+			<boxGeometry args={[1,1,1]} />
+		</mesh>	
+	);
+}
+
 const Calculator: React.FC = () => {
 	return(
-		<p>Calculator</p>
+		<Canvas>
+			<Box position={[0,0,0]} />
+		</Canvas>
 	)
 }
 
