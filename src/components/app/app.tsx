b75366eaa20172232e92892edb3b06d888239226
Pavlienko <computethew@gmail.com>
2022-08-25 16:11:43 +0300

    calc

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -3,7 +3,8 @@ import Calculator from "../calculator";
 const App: React.FC = () => {
   return (
     <div>
-      <h1>SUPER ULTRA MEGA CALCULATOR 3D</h1>
+      {/* <h1>SUPER ULTRA MEGA CALCULATOR 3D</h1> */}
+      <h1>GLSL</h1>
       <Calculator />
     </div>
   );
