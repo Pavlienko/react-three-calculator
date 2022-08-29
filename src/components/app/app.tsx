102307699b97a67bc08784e45c8ca29f94e58a9e
Pavlienko <computethew@gmail.com>
2022-08-29 14:25:24 +0300

    buttons-selection-added

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -8,13 +8,13 @@ const App: React.FC = () => {
     console.log("wow");
   }, [sign]);
 
-  function handleChange(value:string) {
-    setSign(sign + value);
+  function handleChange(value: string) {
+    sign === "GLSL" ? setSign(value) : setSign(sign + value);
   }
   return (
     <div>
       {/* <h1>SUPER ULTRA MEGA CALCULATOR 3D</h1> */}
-      <h1>{sign}</h1>
+      <h1 style={{ userSelect: "none" }}>{sign}</h1>
       <Calculator sign={sign} onChange={handleChange} />
     </div>
   );
--- src/components/calculator-box-geo/calculatorBoxGeo.tsx
+++ src/components/calculator-box-geo/calculatorBoxGeo.tsx
@@ -1,12 +1,9 @@
-import { extend, MeshProps } from "@react-three/fiber";
+import { MeshProps } from "@react-three/fiber";
 import { Html } from "@react-three/drei";
 import * as THREE from "three";
-import { FontLoader } from "three/examples/jsm/loaders/FontLoader";
-import { TextGeometry } from "three/examples/jsm/geometries/TextGeometry";
 import { useRef, useState } from "react";
-import Montserrat from "../../assets/montserrat-medium-regular.json";
 
-extend({ TextGeometry });
+import "./calculatorBoxGeo.css";
 
 type Buttons = {
   text?: string;
@@ -15,14 +12,6 @@ type Buttons = {
 } & MeshProps;
 
 export default function CalculatorBoxGeo(props: Buttons) {
-  // const {buttons} = props;
-  // const font = new FontLoader().parse(Montserrat);
-  // const textOptions = {
-  //   font,
-  //   size: 1,
-  //   height: 0.1,
-  // };
-
   // This reference will give us direct access to the THREE.Mesh object
   const ref = useRef<THREE.Mesh>(null!);
   // Hold state for hovered and clicked events
@@ -35,16 +24,31 @@ export default function CalculatorBoxGeo(props: Buttons) {
       ref={ref}
       castShadow
       receiveShadow
-      scale={[clicked ? 1.9 : 1, 1, 1]}
-      // onClick={(event) => click(!clicked)}
-      onClick={(event) => props.onChange(props.text)}
-      onPointerOver={(event) => hover(true)}
-      onPointerOut={(event) => hover(false)}
+      scale={[clicked ? 1.4 : 1, 1, 1]}
+      onClick={(event) => {
+        props.onChange(props.text);
+        // click(!clicked);
+      }}
+      onPointerOver={(event) => {
+        click(true);
+        hover(true);
+      }}
+      onPointerOut={(event) => {
+        click(false)
+        hover(false)}
+      } 
     >
-      <boxGeometry args={[0.2, 1, 1.1]} />
+      <boxGeometry args={[0.2, 1, props.text === "0" ? 2.2 : 1.1]} />
       <meshStandardMaterial color={hovered ? "red" : "blue"} opacity={0.1} />
-      <Html transform={true} rotation={[0, Math.PI / 2, 0]}>
-        <h2>{props.text}</h2>
+      <Html
+        transform={true}
+        rotation={[0, Math.PI / 2, 0]}
+        position={[0.11, 0.35, -0.01]}
+        occlude={[ref]}
+        className="calc-symbols"
+        wrapperClass="calc-symbols-wrapper"
+      >
+        {props.text}
       </Html>
     </mesh>
   );
